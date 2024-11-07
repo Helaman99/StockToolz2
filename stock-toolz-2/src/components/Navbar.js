89eb7167fd3d80d0429b9817ebe1d29df275d6fb
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2024-11-07 15:31:04 -0800

    Routes Added and Navbar Changed

--- stock-toolz-2/src/components/Navbar.js
+++ stock-toolz-2/src/components/Navbar.js
@@ -1,20 +1,13 @@
 import './components.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return(
         <div id = 'Navbar'>
-            <button id = 'Logo'>StockToolz2</button>
-            <Navbutton text = 'Trade Planner' />
-            <Navbutton text = 'Profit Calculator' />
-            <Navbutton text = 'Cost Basis Total Calculator' />
+            <Link to = '/' id = 'Logo'>StockToolz2</Link>
+            <Link to = '/trade-planner'>Trade Planner</Link>
+            <Link to = '/profit-calculator'>Profit Calculator</Link>
+            <Link to = '/cost-basis-total'>Cost Basis Total Calculator</Link>
         </div>
-    );
-}
-
-function Navbutton({ text }) {
-    return(
-        <button className = 'Navbutton'>
-            <p>{ text }</p>
-        </button>
     );
 }
